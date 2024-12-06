d99b36329e17694c4a02b1d91eb143331733a4c7
EngJao89 <eng_joao@outlook.com>
2024-12-06 13:44:06 -0300

    Initial Commit

---
+++ PokemonGoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonGoApp
+//
+//  Created by João Ricardo Martins Ribeiro on 06/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
